a14ee23d978bfb48147e55868917b6f85d770de9
apurva999 <apurvasomani111@gmail.com>
2023-07-05 02:08:33 +0530

    student form done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import './'
+
 
 function App() {
+  const [inputarr,setinputarr]=useState([])
+  const [inputdata, setinputdata]=useState({
+    name:"",
+    rollNo:""
+  })
+  const changehandle=(e)=>{
+    setinputdata({...inputdata,[e.target.name]:e.target.value})
+   }
+   
+   let {name,rollNo}=inputdata
+   const handlesubmit=()=>{
+    setinputarr([...inputarr,{name,rollNo}])
+    setinputdata({name:"",rollNo:""})
+   }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <input type="text" autoComplete='off' name="name" value={inputdata.name} placeholder='Enter your name' onChange={changehandle}/><br/>  
+          <input type="text" autoComplete='off' name="rollNo" value={inputdata.rollNo} placeholder='RollNo' onChange={changehandle}/><br/>
+          <button onClick={handlesubmit}>Add Info</button>
+   
+          <table border={1} cellPadding={10} width="30%">
+            <tbody>
+            <tr>
+              <td>Name</td>
+              <td>RollNo</td>
+            </tr>
+
+            {
+              inputarr.map((info,ind)=>(
+                  
+                    <tr key={ind}>
+                    <td>{info.name}</td>
+                    <td>{info.rollNo}</td>
+                    </tr>
+                  
+                ))}
+            </tbody>
+          </table>
     </div>
   );
 }
---
+++ src/studentForm.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function studentForm() {
+  return (
+    <div>
+      <input 
+      type="text"
+      
+
+       />
+
+       
+    </div>
+  )
+}
